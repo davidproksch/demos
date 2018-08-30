85e098d8145460fd47f0411cc6a93013e503984c
David Proksch <dproksch@redhat.com>
2018-08-29 20:58:13 -0400

    Add MYSQL Env Var processing

--- warehouse/src/main/java/com/redhat/proksch/demo/warehouse/Warehouse.java
+++ warehouse/src/main/java/com/redhat/proksch/demo/warehouse/Warehouse.java
@@ -4,11 +4,14 @@ import io.vertx.core.Future;
 public class Warehouse extends AbstractVerticle {
     @Override
     public void start(Future<Void> fut) {
+	String s = System.getEnv("MYSQL_USER");
+	s = "<h1>Hello " + s + " from Warehouse!</h1>";
         vertx
             .createHttpServer()
             .requestHandler(r ->
                 r.response()
-                 .end(" <h1>Hello from Warehouse!</h1> "))
+                 .end(s))
+                 //.end(" <h1>Hello from Warehouse!</h1> "))
             .listen(8080, result -> {
                 if (result.succeeded()) {
                     fut.complete();
