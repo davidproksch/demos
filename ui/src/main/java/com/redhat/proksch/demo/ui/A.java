05bb46443c68a6e243b5f5c3e50b8217434945d0
David Proksch <dproksch@redhat.com>
2018-08-31 00:15:36 -0400

    fix syntax error in jsp

--- ui/src/main/java/com/redhat/proksch/demo/ui/A.java
+++ ui/src/main/java/com/redhat/proksch/demo/ui/A.java
@@ -2,5 +2,5 @@ package com.redhat.proksch.demo.ui;
 
 public class A {
 
-	public String getMessage() { return("Hello from a Bean!");}
+	public static String getMessage() { return("Hello from a Bean!");}
 }
